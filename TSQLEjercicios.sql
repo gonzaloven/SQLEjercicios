5b1441b36ef02c47d418606cd22941488aa07c72
gonzaloven <37709434+gonzaloven@users.noreply.github.com>
2019-05-24 14:28:35 -0300

    Subo ejercicio 2

--- TSQLEjercicios.sql
+++ TSQLEjercicios.sql
@@ -22,3 +22,33 @@ AS BEGIN
 		END
 RETURN @respuesta
 END
+								 
+--EJERCICIO 2
+
+CREATE FUNCTION stock_fecha (@prod_codigo char(8), @fecha smalldatetime)
+RETURNS int
+AS
+BEGIN
+  DECLARE @stock_actual decimal(12, 2)
+  DECLARE @vendidos_intervalo decimal(12, 2)
+
+  SELECT @vendidos_intervalo = SUM(
+    CASE
+      WHEN @prod_codigo = item_producto THEN item_cantidad
+      ELSE item_cantidad * comp_cantidad
+    END)
+  FROM Item_Factura JOIN Factura ON fact_numero = item_numero
+    AND fact_sucursal = item_sucursal
+    AND fact_tipo = item_tipo
+  JOIN Producto ON item_producto = item_producto
+  JOIN Composicion ON comp_producto = item_producto
+  WHERE prod_codigo = @prod_codigo
+  AND CONVERT(date, fact_fecha) BETWEEN CONVERT(date, @fecha) AND 
+					CONVERT(date, GETDATE())
+
+  SELECT @stock_actual = SUM(stoc_cantidad)
+  FROM STOCK
+  WHERE @prod_codigo = stoc_producto
+
+  RETURN @stock_actual + @vendidos_intervalo
+END
